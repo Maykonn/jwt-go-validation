3bf94b5faea92e08c6e88da9abfa01006175308f
Maykonn <maykonn@outlook.com>
2018-04-13 15:22:39 -0300

    New methods for parse and decode

--- jwt.go
+++ jwt.go
@@ -6,40 +6,40 @@ import (
 	"strconv"
 )
 
-func JwtSignatureIsValid(signatureString string, tokenString string) (bool) {
-	token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+func JwtParse(signatureString string, tokenString string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		// testing if is HMAC signing method
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method")
 		}
 		return []byte(signatureString), nil
 	})
+	return token, err
+}
+
+func JwtSignatureIsValid(signatureString string, tokenString string) (bool) {
+	token, _ := JwtParse(signatureString, tokenString)
 	return token.Valid
 }
 
-func JwtValidateSignatureAndReturnParsed(signatureString string, tokenString string) (*jwt.Token) {
-	token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// testing if is HMAC signing method
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method")
-		}
-		return []byte(signatureString), nil
-	})
-	return token
+func JwtDecode(token *jwt.Token) (jwt.MapClaims) {
+	return token.Claims.(jwt.MapClaims)
 }
 
 func JwtIdClaimIsValid(signatureString string, tokenString string, id string) (bool, error) {
-	token := JwtValidateSignatureAndReturnParsed(signatureString, tokenString)
-	if idClaim, ok := token.Claims.(jwt.MapClaims)["id"]; ok && token.Valid {
+	if token, _ := JwtParse(signatureString, tokenString); token != nil {
 		// this cast is necessary how explained in this link(the JWT json parser, casting integer to float64):
 		// https://github.com/dgrijalva/jwt-go/pull/162#issuecomment-317074607
 		givenIdAsFloat64, err := strconv.ParseFloat(id, 64)
 		if err != nil {
 			return false, err
 		}
+
+		idClaim := JwtDecode(token)["id"]
 		if idClaim == givenIdAsFloat64 {
-			return true, err
+			return true, nil
 		}
 	}
-	return false, fmt.Errorf("given ID don't match the JWT id claim")
+
+	return false, fmt.Errorf("given id do not matches jwt id")
 }
