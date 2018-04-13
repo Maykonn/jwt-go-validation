ae23d1d8d30a56fb6c359cd57e13b5e9d4a8e728
Maykonn <maykonn@outlook.com>
2018-04-13 14:42:55 -0300

    Added test to decode

--- test/jwttest.go
+++ test/jwttest.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	testValidSignature()
 	testInValidSignature()
+	testDecode()
 	testIdClaim()
 }
 
@@ -31,6 +32,16 @@ func testInValidSignature() {
 	}
 
 	fmt.Println("Test an invalid signature:")
+	fmt.Println(test)
+}
+
+func testDecode() {
+	signatureString := "fd6e28d3186f799458595dd466c8c957daa0a7ba"
+	tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTE0NjUwLCJuYW1lIjoiTWF5a29ubiBXZWxpbmd0b24gQ2FuZGlkbyIsImlhdCI6MTUyMzQ2ODQwMiwiZXhwIjoxNTI2MDYwNDAyLCJpc3MiOiIxNzcuMjAuMjI1LjE2MiIsInN1YiI6IjExNDY1MCJ9.VIqAzk7_eilxxgOFVu_ygCoc2FfyafltyFoMqo2Be7A"
+	claims := jwt_go_validation.JwtDecode(signatureString, tokenString)
+	_, test := claims["id"]
+
+	fmt.Println("Test Decode:")
 	fmt.Println(test)
 }
 
